126e8cb8eb18cb1f994ad173a75dee04af4c11e0
Your Name <umaimajaffer2003@gmail.com>
2024-11-10 23:44:57 +0500

    Your commit message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import "./Home.css"
 export default function Home() {
   return (
@@ -5,15 +6,19 @@ export default function Home() {
       <h1>About Me</h1>
       <div className="content">
         <p>
-          "My name is <strong>UMAIMA JAFFER</strong>, and I am a <strong>Next.js</strong> developer currently
+          My name is <strong>UMAIMA JAFFER</strong>and I am a <strong>Next.js</strong> developer currently
           pursuing a degree in Software Engineering from UIT University. I am
           also enrolled in the Web3.0 course offered through the Governor Sindh
           IT Initiative. I have completed a web development course and possess
           knowledge of <strong>React</strong>, as well as proficiency in <strong>Java</strong>, <strong>Python</strong>, and
           several other programming languages. Software engineering is my
-          passion, and I love programming."
+          passion, and I love programming.
         </p>
-        <img src="./images/img1.jpeg" alt="Umaima Jaffer" />
+        <Image
+            src="/images/img1.jpeg" // Update with your image path
+            alt="Contact Icon"
+            
+          />
       </div>
     </div>
   );
